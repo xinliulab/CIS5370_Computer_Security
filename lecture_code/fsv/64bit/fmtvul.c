be4f22baccf333b76406649f6d382a2608a5f70a
Xin Liu <xinliu1@umbc.edu>
2025-03-20 00:39:08 -0400

    u

---
+++ lecture_code/fsv/64bit/fmtvul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void fmtstr(char *str)
+{
+    void* framep;
+    void* ret;
+
+    // Copy ebp into framep
+    asm ("mov %%rbp, %0" : "=r" (framep));             
+    ret = framep + 8;
+
+    /* print out information for experiment purpose */
+    printf("The address of the input array:  %p\n", str);
+    printf("The value of the frame pointer:  %p\n", framep);
+    printf("The value of the return address: %p\n", *((unsigned long long*)ret));
+
+    printf(str); // The vulnerable place
+
+    printf("The value of the return address: %p\n", *((unsigned long long*)ret));
+}
+
+int main(int argc, char **argv)
+{
+    FILE *badfile;
+    char str[200];
+
+    badfile = fopen("badfile", "rb");
+    fread(str, sizeof(char), 200, badfile);
+    fmtstr(str);
+
+    return 1;
+}
+
